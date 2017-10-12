c688b515feb67b2d0d42ab09e7da21108548f230
solbadguy0696 <toymoon10@gmail.com>
2017-10-12 17:11:50 +0900

    Lesson19 バリデーション機能によって、保存対象にならなかった場合の処理を実装するため、store.phpの条件式を追記しました。

--- store.php
+++ store.php
@@ -3,8 +3,11 @@ require('functions.php');
 //create($_POST);
 //checkReferer();
 $res = checkReferer();
-if ($res == 'index') {
+if ($res != 'back') {
   header('location: ./index.php');  //リダイレクト先の指定を行う
-}
+} else if ($res == 'index') {
+  header('location: ./index.php');
+} else {
   header('location: '.$_SERVER['HTTP_REFERER'].'');  //リダイレクト先の指定を行う
+}
 ?>
