31aafd6d9d36a412961f13bf0a51626079988329
solbadguy0696 <toymoon10@gmail.com>
2017-10-12 16:09:48 +0900

    Lesson16 リファラの確認を行い、リダイレクト先を振り分ける条件式を組み込みました。

--- store.php
+++ store.php
@@ -1,6 +1,10 @@
 <?php
 require('functions.php');
 //create($_POST);
-checkReferer();
-header('location: ./index.php');  //リダイレクト先の指定を行う
+//checkReferer();
+$res = checkReferer();
+if ($res == 'index') {
+  header('location: ./index.php');  //リダイレクト先の指定を行う
+}
+  header('location: '.$_SERVER['HTTP_REFERER'].'');  //リダイレクト先の指定を行う
 ?>
