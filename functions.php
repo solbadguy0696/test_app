3d206b673ce0a555c9ad4cab6824b82593e02c05
solbadguy0696 <toymoon10@gmail.com>
2017-10-12 16:38:45 +0900

    Lesson18 セキュリティ対策のため、sessionを開始する関数や、エスケープ処理、tokenチェック用の関数をfunctions.phpに記述

--- functions.php
+++ functions.php
@@ -1,8 +1,36 @@
 <?php
 require('connection.php');
+session_start();
 
 function create($data) {
-  insertDb($data['todo']);
+  if (checkToken($data['token'])) {
+    insertDb($data['todo']);
+  }
+}
+
+// エスケープ処理
+function h($s) {
+  return htmlspecialchars($s, ENT_QUOTES, "UTF-8");
+}
+
+// sessionに暗号化したtokenを入れる
+function setToken() {
+  $token = sha1(uniqid(mt_rand(), true));
+  $_SESSION['token'] = $token;
+}
+
+// sessionのチェックを行いCSRF対策を行う
+function checkToken($data) {
+  if (empty($_SESSION['token']) || ($_SESSION['token'] != $data)) {
+    $_SESSION['err'] = '不正な操作です';
+    header('location: '.$_SERVER['HTTP_REFERER'].'');
+    exit();
+  }
+  return true;
+}
+
+function unsetSession() {
+  if (!empty($_SESSION['err'])) $_SESSION['err'] = '';
 }
 
 // 全件取得
@@ -12,7 +40,9 @@ function index() {
 
 // 更新
 function update($data) {
-  updateDb($data['id'], $data['todo']);
+  if (checkToken($data['token'])) {
+    updateDb($data['id'], $data['todo']);
+  }
 }
 
 function checkReferer() {
