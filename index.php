a252276f36c18c9fdc391eedd54c9c9b9259ad62
solbadguy0696 <toymoon10@gmail.com>
2017-10-12 14:58:24 +0900

    表示とそれぞれのデータに削除、更新を行うための下準備のため、タグを修正しました。

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+  require('functions.php');
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -19,6 +22,22 @@
         <th>更新</th>
         <th>削除</th>
       </tr>
+      <?php foreach(index() as $todo): ?>
+        <tr>
+          <td><?php echo $todo['id'] ?></td>
+          <td><?php echo $todo['todo'] ?></td>
+          <td>
+            <a href="">更新</a>
+          </td>
+          <td>
+            <form action="store.php" method="POST">
+              <input type="hidden" name="id" value="">
+              <input type="hidden" name="type" value="delete">
+              <button type="submit">削除</button>
+            </form>
+          </td>
+        </tr>
+      <?php endforeach; ?>
     </table>
   </div>
 </body>
